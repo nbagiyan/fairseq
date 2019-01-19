d3cb294096d14ce50dba683f7354687d60db9b30
nbagiyan <nkb997@gmail.com>
2019-01-19 12:26:35 +0300

    fix class name

---
+++ fairseq/criterions/elbo_ls.py
@@ -0,0 +1,83 @@
+import math
+import torch.nn.functional as F
+import torch
+
+from fairseq import utils
+
+from . import FairseqCriterion, register_criterion
+
+
+@register_criterion('elbo_ls')
+class ELBO_LS(FairseqCriterion):
+
+    def __init__(self, args, task):
+        super().__init__(args, task)
+        self.alpha = args.alpha
+
+    @staticmethod
+    def add_args(parser):
+        """Add criterion-specific arguments to the parser."""
+        # fmt: off
+        parser.add_argument('--alpha', default=79, type=float, metavar='D',
+                            help='alpha for cross-entropy coefficient')
+        parser.add_argument('--label-smoothing', default=0., type=float, metavar='D',
+                            help='epsilon for label smoothing, 0 means no label smoothing')
+        # fmt: on
+
+    def forward(self, model, sample, reduce=True):
+        """Compute the loss for the given sample.
+
+        Returns a tuple with three elements:
+        1) the loss
+        2) the sample size, which is used as the denominator for the gradient
+        3) logging outputs to display while training
+        """
+        net_output = model(**sample['net_input'])
+        ls_cross_entropy, nll_loss = self.compute_ls_cross_entropy(model, net_output, sample, reduce=reduce)
+        kld = self.compute_kld(net_output)
+        loss = self.alpha * ls_cross_entropy + kld
+        sample_size = sample['target'].size(0) if self.args.sentence_avg else sample['ntokens']
+        logging_output = {
+            'loss': utils.item(loss.data) if reduce else loss.data,
+            'nll_loss': utils.item(nll_loss.data) if reduce else nll_loss.data,
+            'kld': utils.item(kld.data) if reduce else kld.data,
+            'ntokens': sample['ntokens'],
+            'nsentences': sample['target'].size(0),
+            'sample_size': sample_size,
+        }
+        return loss, sample_size, logging_output
+
+    def compute_kld(self, net_output):
+        """Compute the kullback leibler divergence for the given sample."""
+        logvar, mu = net_output[2], net_output[3]
+        kld = (-0.5 * torch.sum(logvar - torch.pow(mu, 2) - torch.exp(logvar) + 1, 1)).mean().squeeze()
+        return kld
+
+    def compute_ls_cross_entropy(self, model, net_output, sample, reduce=True):
+        lprobs = model.get_normalized_probs(net_output, log_probs=True)
+        lprobs = lprobs.view(-1, lprobs.size(-1))
+        target = model.get_targets(sample, net_output).view(-1, 1)
+        non_pad_mask = target.ne(self.padding_idx)
+        nll_loss = -lprobs.gather(dim=-1, index=target)[non_pad_mask]
+        smooth_loss = -lprobs.sum(dim=-1, keepdim=True)[non_pad_mask]
+        if reduce:
+            nll_loss = nll_loss.sum()
+            smooth_loss = smooth_loss.sum()
+        eps_i = self.eps / lprobs.size(-1)
+        loss = (1. - self.eps) * nll_loss + eps_i * smooth_loss
+        return loss, nll_loss
+
+
+def aggregate_logging_outputs(logging_outputs):
+    """Aggregate logging outputs from data parallel training."""
+    ntokens = sum(log.get('ntokens', 0) for log in logging_outputs)
+    nsentences = sum(log.get('nsentences', 0) for log in logging_outputs)
+    sample_size = sum(log.get('sample_size', 0) for log in logging_outputs)
+    return {
+        'loss': sum(log.get('loss', 0) for log in logging_outputs) / sample_size / math.log(2),
+        'nll_loss': sum(log.get('nll_loss', 0) for log in logging_outputs) / ntokens / math.log(2),
+        'kld': sum(log.get('kld', 0) for log in logging_outputs) / sample_size / math.log(2),
+        'ntokens': ntokens,
+        'nsentences': nsentences,
+        'sample_size': sample_size,
+    }
