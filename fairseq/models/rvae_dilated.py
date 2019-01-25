824290868ada28355887003d3c48ced595d506af
nbagiyan <nkb997@gmail.com>
2019-01-25 18:44:24 +0300

    add VAE with dilated convolutions

---
+++ fairseq/models/rvae_dilated.py
@@ -0,0 +1,209 @@
+import torch.nn as nn
+import torch.functional as F
+import torch
+from fairseq import utils
+from fairseq.models import FairseqEncoder
+from fairseq.models import FairseqDecoder
+from fairseq.models import FairseqModel, register_model
+from fairseq.models import register_model_architecture
+
+
+class VAELSTMEncoder(FairseqEncoder):
+
+    def __init__(
+        self, args, dictionary, embed_dim=128, hidden_dim=128, dropout=0.1,
+    ):
+        super().__init__(dictionary)
+        self.args = args
+
+        # Our encoder will embed the inputs before feeding them to the LSTM.
+        self.embed_tokens = nn.Embedding(
+            num_embeddings=len(dictionary),
+            embedding_dim=embed_dim,
+            padding_idx=dictionary.pad(),
+        )
+        self.dropout = nn.Dropout(p=dropout)
+
+        self.hidden_dim = hidden_dim
+
+        # We'll use a single-layer, unidirectional LSTM for simplicity.
+        self.lstm = nn.LSTM(
+            input_size=embed_dim,
+            hidden_size=hidden_dim,
+            num_layers=1,
+            bidirectional=True,
+        )
+
+        self.context_to_mu = nn.Linear(hidden_dim * 2, hidden_dim)
+        self.context_to_logvar = nn.Linear(hidden_dim * 2, hidden_dim)
+
+    def forward(self, src_tokens, src_lengths):
+        # The inputs to the ``forward()`` function are determined by the
+        # Task, and in particular the ``'net_input'`` key in each
+        # mini-batch. We discuss Tasks in the next tutorial, but for now just
+        # know that *src_tokens* has shape `(batch, src_len)` and *src_lengths*
+        # has shape `(batch)`.
+
+        # Note that the source is typically padded on the left. This can be
+        # configured by adding the `--left-pad-source "False"` command-line
+        # argument, but here we'll make the Encoder handle either kind of
+        # padding by converting everything to be right-padded.
+        if self.args.left_pad_source:
+            # Convert left-padding to right-padding.
+            src_tokens = utils.convert_padding_direction(
+                src_tokens,
+                padding_idx=self.dictionary.pad(),
+                left_to_right=True
+            )
+
+        bsz, seqlen = src_tokens.size()
+
+        # Embed the source.
+        x = self.embed_tokens(src_tokens)
+
+        # Apply dropout.
+        x = self.dropout(x)
+
+        # Pack the sequence into a PackedSequence object to feed to the LSTM.
+        x = nn.utils.rnn.pack_padded_sequence(x, src_lengths, batch_first=True)
+
+        # Get the output from the LSTM.
+        _outputs, (_final_hidden, _final_cell) = self.lstm(x)
+
+        x, _ = nn.utils.rnn.pad_packed_sequence(_outputs, padding_value=0)
+
+        assert list(x.size()) == [seqlen, bsz, 2*self.hidden_dim]
+
+        final_hidden = torch.mean(x, dim=0)
+
+        assert list(final_hidden.size()) == [bsz, 2*self.hidden_dim]
+
+        mu = self.context_to_mu(final_hidden)
+        logvar = self.context_to_logvar(final_hidden)
+
+        std = torch.exp(0.5 * logvar)
+        z = torch.randn(mu.size()).cuda()
+        z = z * std + mu
+        # Return the Encoder's output. This can be any object and will be
+        # passed directly to the Decoder.
+        return {
+            # this will have shape `(bsz, hidden_dim)`
+            'final_hidden': z,
+            'logvar': logvar,
+            'mu': mu
+        }
+
+    # Encoders are required to implement this method so that we can rearrange
+    # the order of the batch elements during inference (e.g., beam search).
+    def reorder_encoder_out(self, encoder_out, new_order):
+        """
+        Reorder encoder output according to `new_order`.
+
+        Args:
+            encoder_out: output from the ``forward()`` method
+            new_order (LongTensor): desired order
+
+        Returns:
+            `encoder_out` rearranged according to `new_order`
+        """
+        final_hidden = encoder_out['final_hidden']
+        return {
+            'final_hidden': final_hidden.index_select(0, new_order),
+        }
+
+
+class DilatedConvolutionsDecoder(FairseqDecoder):
+
+    def __init__(
+        self, args, dictionary, embed_dim=128, hidden_dim=128, dropout=0.1,
+    ):
+        super().__init__(dictionary)
+        self.args = args
+
+        self.kernels = [(1, 25), (2, 50), (3, 75), (4, 100), (5, 125), (6, 150)]
+
+        self.decoder_dilations = [1, 2, 4]
+
+        self.decoder_kernels = [(400, hidden_dim + embed_dim, 3),
+                                (450, 400, 3),
+                                (500, 450, 3)]
+
+        self.decoder_num_layers = len(self.decoder_kernels)
+
+        self.decoder_paddings = [DilatedConvolutionsDecoder.effective_k(w, self.decoder_dilations[i]) - 1
+                                 for i, (_, _, w) in enumerate(self.decoder_kernels)]
+
+        self.dropout = nn.Dropout(p=dropout)
+
+        self.embed_tokens = nn.Embedding(
+            num_embeddings=len(dictionary),
+            embedding_dim=embed_dim,
+            padding_idx=dictionary.pad(),
+        )
+
+        self.kernels = [nn.Parameter(torch.Tensor(out_chan, in_chan, width).normal_(0, 0.05))
+                        for out_chan, in_chan, width in self.decoder_kernels]
+
+        self._add_to_parameters(self.kernels, 'decoder_kernel')
+
+        self.biases = [nn.Parameter(torch.Tensor(out_chan).normal_(0, 0.05))
+                       for out_chan, in_chan, width in self.decoder_kernels]
+
+        self._add_to_parameters(self.biases, 'decoder_bias')
+
+        self.out_size = self.decoder_kernels[-1][0]
+
+        self.output_projection = nn.Linear(self.out_size, len(dictionary))
+
+    def forward(self, prev_output_tokens, encoder_out):
+
+        final_encoder_hidden = encoder_out['final_hidden']
+
+        bsz, tgt_len = prev_output_tokens.size()
+
+        # Embed the source.
+        x = self.embed_tokens(prev_output_tokens)
+
+        # Apply dropout.
+        x = self.dropout(x)
+
+        x = torch.cat(
+            [x, final_encoder_hidden.unsqueeze(1).expand(bsz, tgt_len, -1)],
+            dim=2,
+        )
+
+        x = x.transpose(1, 2).contiguous()
+
+        for layer, kernel in enumerate(self.kernels):
+            # apply conv layer with non-linearity and drop last elements of sequence to perfrom input shifting
+            x = F.conv1d(x, kernel,
+                         bias=self.biases[layer],
+                         dilation=self.decoder_dilations[layer],
+                         padding=self.decoder_paddings[layer])
+
+            x_width = x.size()[2]
+            x = x[:, :, :(x_width - self.decoder_paddings[layer])].contiguous()
+
+            x = F.relu(x)
+
+        x = x.transpose(1, 2).contiguous()
+
+        x = x.view(-1, self.out_size)
+
+        x = self.fc(x)
+
+        return x, None
+
+
+    @staticmethod
+    def effective_k(k, d):
+        """
+        :param k: kernel width
+        :param d: dilation size
+        :return: effective kernel width when dilation is performed
+        """
+        return (k - 1) * d + 1
+
+    def _add_to_parameters(self, parameters, name):
+        for i, parameter in enumerate(parameters):
+            self.register_parameter(name='{}-{}'.format(name, i), param=parameter)
