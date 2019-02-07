511c435811d4a76d44ab9a4dfdf75806ae253369
nbagiyan <nkb997@gmail.com>
2019-02-07 11:45:28 +0300

    bug fix

--- fairseq/models/rvae_bytenet.py
+++ fairseq/models/rvae_bytenet.py
@@ -205,7 +205,9 @@ class ByteNetDecoder(FairseqDecoder):
             [x, final_encoder_hidden.unsqueeze(1).expand(bsz, tgt_len, -1)],
             dim=2,
         )
-        print(x.size())
+
+        x = x.transpose(2, 1).transpose(0, 2)
+
         for layer in self.layers:
             x = layer(x)
 
