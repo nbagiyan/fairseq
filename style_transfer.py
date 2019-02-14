dab3ed273ec2d2f810bb698e6d00ce2caae4802e
nbagiyan <nkb997@gmail.com>
2019-02-14 21:37:11 +0300

    bug fix in using args

--- style_transfer.py
+++ style_transfer.py
@@ -107,7 +107,7 @@ def main(args):
             sampling=args.sampling, sampling_topk=args.sampling_topk, sampling_temperature=args.sampling_temperature,
             diverse_beam_groups=args.diverse_beam_groups, diverse_beam_strength=args.diverse_beam_strength,
             match_source_len=args.match_source_len, no_repeat_ngram_size=args.no_repeat_ngram_size,
-            attack=True, target=target, classifier=classifier, epsilon=args.fgsm_epsilon, num_iter=args.num_iter
+            attack=True, target=target, classifier=classifier, epsilon=args.fgsm_epsilon, num_iter=args.fgsm_iter
         )
 
     if use_cuda:
